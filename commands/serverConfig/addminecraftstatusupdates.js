d2b6ae90de5276fc7da35e70ead92b70292a317e
put37585 <thomas.puerner@st.oth-regensburg.de>
2021-04-22 21:39:14 +0200

    Allserver status change i cant type...

--- commands/serverConfig/addminecraftstatusupdates.js
+++ commands/serverConfig/addminecraftstatusupdates.js
@@ -18,7 +18,7 @@ module.exports = {
             if (dbErrorSelect || dbResponseSelect.rows > 1) {
                 return console.log("ERROR select server " + servername)
             }
-            if (dbErrorSelect.rows.length == 0) {
+            if (dbResponseSelect.rows.length == 0) {
                 return receivedMessage.reply("No server with this name.")
             }
             dbClient.query("INSERT INTO mcservernotifylist (mcservername, guildid ,channelid, msgid) VALUES($1,$2,$3,$4) ON CONFLICT (mcservername, guildid) DO UPDATE SET channelid = $3 , msgid = $4", [servername, guildID, channelID, null], function (dbErrorInsert, dbResponseInsert) {
