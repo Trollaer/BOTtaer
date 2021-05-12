3853fe940c2a7def84346ccdfccf0fc306c625a8
Thomas Puerner <thomas.puerner@st.oth-regensburg.de>
2021-05-12 17:43:29 +0200

    bugfix

--- bot.js
+++ bot.js
@@ -287,29 +287,29 @@ async function allServerStatusUpdate(status) {
 if (SERVER_IP) {
     console.log("Pinging " + SERVER_IP);
     const ping = require('ping');
-    var lastIsAlive = true;
+    var lastIsAlive = false;
     setInterval(function () {
         ping.sys.probe(SERVER_IP, function (isAlive) {
             console.log("Pinged Server : "+ isAlive);
             if (lastIsAlive === isAlive) { //was and is still offline/online
-                //console.log("Same status")
+                console.log("Same status")
                 return;
             }
             if (lastIsAlive && !isAlive) { // was online, is now offline
-                allServerStatusUpdate("offline");
+                //allServerStatusUpdate("offline");
                 //message some one
-                //console.log("turned on")
+                console.log("turned on")
                 lastIsAlive = isAlive;
                 return;
             }
             if (!lastIsAlive && isAlive) { // was offline, is now online
                 // allServerStatusUpdate("online"); // nicht alle online schalten
-                // console.log("turned off")
+                console.log("turned off")
                 lastIsAlive = isAlive;
                 return;
             }
         });
-    },  900000);
+    }, 30000 );//900000
 
 }
 
